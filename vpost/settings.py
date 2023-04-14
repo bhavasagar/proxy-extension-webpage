0ab1cb6396f565d5f559eeb8c66026d3888531f1
bhavasagar09 <bhavasagar.putta@quantrium.ai>
2023-04-14 11:38:09 +0530

    "env"

--- vpost/settings.py
+++ vpost/settings.py
@@ -14,11 +14,12 @@ from pathlib import Path
 import os
 import environ
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # BASE_DIR = os.path.dirname(os.path.dirname(
 #    os.path.dirname(os.path.abspath(__file__))))
